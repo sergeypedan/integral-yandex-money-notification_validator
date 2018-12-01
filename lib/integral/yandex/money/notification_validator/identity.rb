cf77b61c5c657bc7d6b431bb8d1b36cee6aa67c5
Sergey Pedan <sergey.pedan@gmail.com>
2018-12-02 00:42:38 +0300

    Added Gemsmith skeleton.
    Built with Gemsmith 12.3.0.

---
+++ lib/integral/yandex/money/notification_validator/identity.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Integral
+  module Yandex
+    module Money
+      module NotificationValidator
+        # Gem identity information.
+        module Identity
+          def self.name
+            "integral-yandex-money-notification_validator"
+          end
+
+          def self.label
+            "Integral Yandex Money Notification Validator"
+          end
+
+          def self.version
+            "0.1.0"
+          end
+
+          def self.version_label
+            "#{label} #{version}"
+          end
+        end
+      end
+    end
+  end
+end
